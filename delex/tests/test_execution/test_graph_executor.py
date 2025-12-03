9105e768ac5ceef4ad4c49845308c1910526742e
Dev Ahluwalia <devahluwalia34@gmail.com>
2025-12-02 18:11:31 -0600

    removed non-useful test

--- delex/tests/test_execution/test_graph_executor.py
+++ delex/tests/test_execution/test_graph_executor.py
@@ -21,33 +21,3 @@ class TestGraphExecutor:
         assert executor.index_table is not None
         assert executor.search_table is not None
         assert executor.index_table_count == 1000
-
-    def test_graph_executor_topological_order(self):
-        """Test that nodes are executed in topological order."""
-        from delex.graph import PredicateNode
-        from delex.graph.algorithms import topological_sort
-        from delex.lang import ExactMatchPredicate
-        node1 = PredicateNode(ExactMatchPredicate(index_col='_id', search_col='_id', invert=False))
-        node2 = PredicateNode(ExactMatchPredicate(index_col='_id', search_col='_id', invert=False))
-        node3 = PredicateNode(ExactMatchPredicate(index_col='_id', search_col='_id', invert=False))
-        node4 = PredicateNode(ExactMatchPredicate(index_col='_id', search_col='_id', invert=False))
-        node5 = PredicateNode(ExactMatchPredicate(index_col='_id', search_col='_id', invert=False))
-        node6 = PredicateNode(ExactMatchPredicate(index_col='_id', search_col='_id', invert=False))
-        node7 = PredicateNode(ExactMatchPredicate(index_col='_id', search_col='_id', invert=False))
-        node1.add_out_edge(node2)
-        node1.add_out_edge(node3)
-        node3.add_out_edge(node6)
-        node3.add_out_edge(node4)
-        node4.add_out_edge(node7)
-        node5.add_out_edge(node7)
-        topo_sorted = topological_sort(node7)
-        expected_nodes = {id(node1), id(node3), id(node4), id(node5), id(node7)}
-        actual_nodes = {id(n) for n in topo_sorted}
-        # node6 should not be in the sorted list, since it is not connected to node7
-        assert id(node6) not in actual_nodes
-        assert actual_nodes == expected_nodes
-        index = {n: i for i, n in enumerate(topo_sorted)}
-        assert index[node1] < index[node3]
-        assert index[node3] < index[node4]
-        assert index[node5] < index[node7]
-        assert index[node4] < index[node7]
