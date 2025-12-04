6b9fcf09c46aef3d1960cf919d896b4419e2478a
Dev Ahluwalia <devahluwalia34@gmail.com>
2025-12-03 18:05:53 -0600

    test cost estimation test coverage at 84%

--- delex/tests/test_execution/test_cost_estimation.py
+++ delex/tests/test_execution/test_cost_estimation.py
@@ -64,11 +64,8 @@ class TestCostEstimator:
     """Tests for CostEstimator class."""
 
     @pytest.mark.requires_spark
-    def test_cost_estimator_init(self, spark_session):
+    def test_cost_estimator_init(self, table_a, table_b):
         """Test CostEstimator initialization."""
-        from pyspark.sql import DataFrame
-        table_a = spark_session.createDataFrame([(i,) for i in range(1000)], ['_id'])
-        table_b = spark_session.createDataFrame([(i,) for i in range(1000)], ['_id'])
         nthreads = 4
         estimator = CostEstimator(table_a, table_b, nthreads)
         assert estimator is not None
@@ -78,23 +75,69 @@ class TestCostEstimator:
         assert estimator._table_b_count == 1000
         assert estimator._nthreads == nthreads
 
+    def test_cost_estimator_properties(self, table_a, table_b):
+        nthreads = 4
+        estimator = CostEstimator(table_a, table_b, nthreads)
+        assert estimator is not None
+        assert estimator.table_a_count == 1000
+        assert estimator.nthreads == 4
+        assert estimator.table_b_count == 1000
+
+    def test_cost_estimator_times_properties(self, table_a, table_b):
+        from delex.lang.predicate import ExactMatchPredicate
+        nthreads = 4
+        estimator = CostEstimator(table_a, table_b, nthreads)
+        pred = ExactMatchPredicate(
+            index_col='_id', search_col='_id', invert=False)
+        estimator._search_time[pred] = 0.001
+        estimator._build_time[pred] = 0.001
+        estimator._filter_time[pred.sim] = 0.001
+        assert estimator.search_time(pred) == 0.001
+        assert estimator.build_time(pred) == 0.001
+        assert estimator.filter_time(pred) == 0.001
+
     @pytest.mark.requires_spark
-    def test_cost_estimator_estimate(self, spark_session):
+    def test_cost_estimator_estimate(self, table_a, table_b):
         """Test cost estimation for a node."""
         from delex.lang.predicate import ExactMatchPredicate
         from delex.graph import PredicateNode
-        pred = ExactMatchPredicate(index_col='_id', search_col='_id', invert=False)
+        pred = ExactMatchPredicate(
+            index_col='_id', search_col='_id', invert=False)
         node = PredicateNode(pred)
         from delex.execution.cost_estimation import CostEstimator
-        table_a = spark_session.createDataFrame([(i,) for i in range(1000)], ['_id'])
-        table_b = spark_session.createDataFrame([(i,) for i in range(1000)], ['_id'])
         estimator = CostEstimator(table_a, table_b, nthreads=4)
-        
+
         # Manually set required values for the test
         estimator._selectivity[pred] = 0.1
         estimator._search_time[pred] = 0.001
-        
+
         cost = estimator.estimate_plan_cost(node)
         assert cost is not None
         assert isinstance(cost, float)
         assert cost > 0.0
+
+    def test_cost_estimator_compute_estimates(self, table_a, table_b):
+        """
+       Test cost estimator compute estimates method.
+        """
+        from delex.lang.predicate import ExactMatchPredicate
+        from delex.lang import KeepRule, BlockingProgram
+        estimator = CostEstimator(table_a, table_b, nthreads=4)
+        pred = ExactMatchPredicate(
+            index_col='title', search_col='title', invert=False)
+        keep_rule = KeepRule(predicates=[pred])
+        simple_blocking_program = BlockingProgram(
+            keep_rules=[keep_rule], drop_rules=[])
+        estimator.compute_estimates(simple_blocking_program)
+        assert estimator is not None
+        assert estimator._selectivity[pred] is not None
+        assert estimator._search_time[pred] is not None
+        assert estimator._build_time[pred] is not None
+        assert estimator._filter_time[pred.sim] is not None
+        index_key = pred._get_index_key(for_search=True)
+        assert estimator._index_size[index_key] is not None
+        assert estimator._selectivity[pred] >= 0.0
+        assert estimator._search_time[pred] >= 0.0
+        assert estimator._build_time[pred] >= 0.0
+        assert estimator._filter_time[pred.sim] >= 0.0
+        assert isinstance(estimator._index_size[index_key], ScalingModel)
